fe6c65f7090cb0d7765f942644de2accb12c83e2
Marcin Kulisz <marcin@bashton.com>
2014-08-11 08:56:27 +0100

    Initial change for osfamily = Debian

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,6 +17,18 @@ class graylog2::params {
       $web_config_file       = '/etc/graylog2/web-interface/graylog2-web-interface.conf'
       $web_sysconfig_file    = '/etc/sysconfig/graylog2-web-interface'
     }
+    'Debian': {
+      $es_repo_version       = '0.90'
+      $server_package_name   = 'graylog2-server'
+      $server_service_name   = 'graylog2-server'
+      $server_config_file    = '/etc/graylog2/server/server.conf'
+      $server_sysconfig_file = '/etc/defaults/graylog2-server'
+      $server_plugin_dir     = '/opt/graylog2-server/plugin'
+      $web_package_name      = 'graylog2-web'
+      $web_service_name      = 'graylog2-web'
+      $web_config_file       = '/etc/graylog2/web/graylog2-web-interface.conf'
+      $web_sysconfig_file    = '/etc/defaults/graylog2-web'
+    }
     default: {
       fail("${::operatingsystem} not supported")
       # But pull requests are gratefully accepted.. :)
